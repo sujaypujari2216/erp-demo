712ebe44d1be3de7943ccb2d01a8ec30e478bb51
omkarnangnure26 <40233799+omkarnangnure@users.noreply.github.com>
2020-05-15 11:48:02 +0530

    "table in front cms"

--- src/app/Frontcms/event/event.component.ts
+++ src/app/Frontcms/event/event.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DatatableService } from 'src/app/shared/datatableservice/datatable.service';
+
 
 @Component({
   selector: 'app-event',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventComponent implements OnInit {
 
-  constructor() { }
+  constructor(private datatableservice:DatatableService) { }
 
   ngOnInit(): void {
+    this.datatableservice.initTable("Display event");
   }
 
 }
