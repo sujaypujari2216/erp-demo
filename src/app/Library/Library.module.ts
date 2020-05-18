3d271357af328967cf7e83cb0303b256bac22f99
softiusprime <61870365+softiusprime@users.noreply.github.com>
2020-05-18 11:33:02 +0530

    changes

--- src/app/Library/Library.module.ts
+++ src/app/Library/Library.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Issue1Component } from './issue-return/issue1/issue1.component';
 
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [Issue1Component],
   imports: [
     CommonModule
   ]
