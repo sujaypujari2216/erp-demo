3010b365b2295c7db7761a368add1e4a3a437bd9
omkar9405 <omkarp1101@rediffmail.com>
2020-05-26 01:00:51 +0530

    one

--- src/app/Hostel/hostel.module.ts
+++ src/app/Hostel/hostel.module.ts
@@ -1,11 +1,14 @@
  import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+	FormsModule,
+	ReactiveFormsModule
   ]
 })
 export class HostelModule { }
