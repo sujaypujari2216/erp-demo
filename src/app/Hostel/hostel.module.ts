eb10434732c941eaf6d809e2ea1b4b15483f8cd9
omkar9405 <omkarp1101@rediffmail.com>
2020-05-26 12:51:34 +0530

    ApiRunSucc

--- src/app/Hostel/hostel.module.ts
+++ src/app/Hostel/hostel.module.ts
@@ -2,13 +2,18 @@
 import { CommonModule } from '@angular/common';
 import { FormsModule } from "@angular/forms";
 import { ReactiveFormsModule } from "@angular/forms";
+import { AddHostelComponent } from './add-hostel/add-hostel.component';
+import { AddRoomTypeComponent } from './add-room-type/add-room-type.component';
+import { AddRoomsComponent } from './add-rooms/add-rooms.component';
 
+import { HostelRoutingModule } from './hostel-routing.module';
 @NgModule({
-  declarations: [],
+  declarations: [AddHostelComponent,AddRoomTypeComponent,AddRoomsComponent],
   imports: [
-    CommonModule,
+  CommonModule,
 	FormsModule,
-	ReactiveFormsModule
+  ReactiveFormsModule,
+  HostelRoutingModule
   ]
 })
 export class HostelModule { }
