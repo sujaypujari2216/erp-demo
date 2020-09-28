1d754f0a476e165e99c712feec9928ed99262c74
softiusprime <61870365+softiusprime@users.noreply.github.com>
2020-09-28 20:06:20 +0530

    assign permission

--- src/app/system_setting/system-routing.module.ts
+++ src/app/system_setting/system-routing.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { SesstionSettingComponent } from './sesstion-setting/sesstion-setting.component';
 import { RolesPermissionComponent } from './roles-permission/roles-permission.component';
+import { AssignPermissionComponent } from './assign-permission/assign-permission.component';
+
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -10,6 +12,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 const routes: Routes = [
     { path: 'sesstion-setting', component: SesstionSettingComponent },
     { path: 'roles-permission', component: RolesPermissionComponent },
+    { path: 'assign-permission', component: AssignPermissionComponent },
+
 
    
 ];
--- src/app/system_setting/system.module.ts
+++ src/app/system_setting/system.module.ts
@@ -5,11 +5,12 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { SystemRoutingModule } from './system-routing.module';
 import { SesstionSettingComponent } from './sesstion-setting/sesstion-setting.component';
 import { RolesPermissionComponent } from './roles-permission/roles-permission.component';
+import { AssignPermissionComponent } from './assign-permission/assign-permission.component';
 
 
 
 @NgModule({
-    declarations: [SesstionSettingComponent, RolesPermissionComponent],
+    declarations: [SesstionSettingComponent, RolesPermissionComponent, AssignPermissionComponent],
     imports: [
         CommonModule,
         SystemRoutingModule,
